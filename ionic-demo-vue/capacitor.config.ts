c3b80126312d1b5d26107d58d2d602e9a38b455d
alex <907785012@qq.com>
2025-07-25 16:45:08 +0800

    add ionic-v8 and capacitor-v7

---
+++ ionic-demo-vue/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.demo.vue',
+  appName: 'ionic-demo-vue',
+  webDir: 'dist'
+};
+
+export default config;
